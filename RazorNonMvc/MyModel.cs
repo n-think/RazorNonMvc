d01a5bb2d0644abdc2eff2ead6ce2aa8e705b0df
Kirill K <kirill3611854@gmail.com>
2019-04-20 00:04:38 +0300

    initial

---
+++ RazorNonMvc/MyModel.cs
@@ -0,0 +1,8 @@
+namespace RazorNonMvc
+{
+    public class MyModel
+    {
+        public string String { get; set; }
+        public int Int { get; set; }
+    }
+}
---
+++ RazorNonMvc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RazorNonMvc
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = AppTemplates.RenderTemplate("myTemplate.cshtml", new MyModel() {String = "asd", Int = 3},
+                out string error);
+            Console.Write(result);
+            Console.Write(error);
+            Console.ReadKey();
+
+        }
+    }
+}
